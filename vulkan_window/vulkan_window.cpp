75e4d984cd0bb25f313251bd81c885770c2b79a2
Salmaan Saeed <salsaeed999@gmail.com>
2024-07-30 13:41:21 -0400

    first commit

---
+++ vulkan_window/vulkan_window.cpp
@@ -0,0 +1,40 @@
+// vulkan_window.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIUS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
+
