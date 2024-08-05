da98f008dbd4767e0ebc8971bbc4d72b8d7daeac
Salmaan Saeed <salsaeed999@gmail.com>
2024-08-04 20:32:57 -0400

    the run script invokes the msbuild command to use the clang compiler

--- vulkan_window/run.bat
+++ vulkan_window/run.bat
@@ -1,3 +1,7 @@
+:: Clean build
+MSBuild.exe ..\vulkan_window.sln /p:CLToolExe=clang-cl -target:Clean
+
 :: Build and run the program
-MSBuild.exe ..\vulkan_window.sln
+MSBuild.exe ..\vulkan_window.sln /p:CLToolExe=clang-cl
 ..\x64\Debug\vulkan_window.exe
+
