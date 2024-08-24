14ffe056e44d43ca6a975ec5427118060d30e136
Salmaan Saeed <salsaeed999@gmail.com>
2024-08-24 00:38:43 -0400

    the lint script automatically finds all of the cpp and hpp files recursively

--- lint_codebase.ps1
+++ lint_codebase.ps1
@@ -1,1 +1,9 @@
-clang-tidy -format-style=file ../src/vulkan_window.cpp ../src/triangle_application.cpp ../src/triangle_application.hpp
+$cpp_files = Get-ChildItem -Path ..\src -Filter *.cpp -Recurse | ForEach-Object {
+    "..\src\" + [System.IO.Path]::GetFileName($_)
+}
+
+$hpp_files = Get-ChildItem -Path ..\src -Filter *.hpp -Recurse | ForEach-Object {
+    "..\src\" + [System.IO.Path]::GetFileName($_)
+}
+
+clang-tidy -format-style=file $cpp_files $hpp_files
